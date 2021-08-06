5e20e89a49bc3b75e162224d04c7de99d8441e95
Michael Griebling <sam@c-inspirations.com>
2021-08-06 06:50:19 -0400

    Changed how matched and mismatched cards are indicated.

--- Set/SetCardView.swift
+++ Set/SetCardView.swift
@@ -15,15 +15,18 @@ struct SetCardView: View {
         VStack {
             let cardShape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
             ZStack {
-                let matchColor = card.isMatched ? .yellow : DrawingConstants.cardColour
-                let color = card.failedMatch ? Color.red : matchColor
-                cardShape.fill().foregroundColor(color)
+                cardShape.fill().foregroundColor(DrawingConstants.cardColour)
                 cardShape.strokeBorder(lineWidth: card.isSelected ? 2*DrawingConstants.lineWidth : DrawingConstants.lineWidth)
                 VStack {
                     ForEach([Int](1...card.content.number), id: \.self) { _ in
                         ShapeSetGame.draw(card, colour: DrawingConstants.cardColour)
                     }
                 }.padding()
+                Image(systemName: card.isMatched ? "checkmark" : "xmark")
+                    .font(.system(size: DrawingConstants.xmarkSize))
+                    .foregroundColor(card.isMatched ? .green : .red)
+                    .opacity((card.failedMatch || card.isMatched) ? 1 : 0)
+                    .shadow(radius: 10)
             }
         }.foregroundColor(backColor)
     }
@@ -33,6 +36,7 @@ struct SetCardView: View {
         static let cornerRadius = CGFloat(10)
         static let lineWidth = CGFloat(3)
         static let opacity = 0.8
+        static let xmarkSize = CGFloat(150)
     }
 }
 
